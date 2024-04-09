ca705226654d40bd42540fd6536d232b024a41b3
RNubla <rnubla97@gmail.com>
2024-04-08 20:51:09 -0400

    install tailwind and nextui

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -21,7 +22,9 @@ if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
 		<StrictMode>
-			<RouterProvider router={router} />
+			<NextUIProvider>
+				<RouterProvider router={router} />
+			</NextUIProvider>
 		</StrictMode>,
 	);
 }
